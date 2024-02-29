6ab3b0e753d5504a67e8b278be3c6aaea5c607fc
BaumianerNiklas <o.niklas@tuta.io>
2024-02-29 21:53:02 +0100

    allow reordering modules within a semester

--- src/components/SemesterRow.vue
+++ src/components/SemesterRow.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { modules as allModules, editModule, sortModules } from "@/data/modules.js";
 import ModuleCard from "./ModuleCard.vue";
-import { computed, watch } from "vue";
+import { computed } from "vue";
 import FormModal from "./FormModal.vue";
 import ModuleForm from "./ModuleForm.vue";
 import semesterDropzoneHandler from "@/util/semesterDropzoneHandler";
@@ -11,27 +11,31 @@ const { semester } = defineProps<{ semester: number }>();
 const modules = computed(() => allModules.value.filter((m) => m.semester === semester));
 const totalEcts = computed(() => modules.value.reduce((acc, curr) => curr.ects + acc, 0));
 
-watch(modules, () => sortModules(modules.value));
+function handleDrop(event: DragEvent, sortIndex?: number) {
+	semesterDropzoneHandler(event, semester, sortIndex);
+	if (modules.value.length >= 2) sortModules(modules.value);
+}
 </script>
 
 <template>
-	<div draggable="true" @dragover.prevent @drop="(ev) => semesterDropzoneHandler(ev, semester)">
+	<div>
 		<span>{{ semester }}. semester ({{ totalEcts }})</span>
 		<div class="flex">
-			<FormModal
-				reset-on-close
-				v-for="module in modules"
-				:key="module.id"
-				@submit="(data) => editModule(module, data)"
-			>
-				<template v-slot:form-elements>
-					<ModuleForm :module="module" />
-				</template>
+			<div v-for="(module, index) in sortModules(modules)" :key="module.id" class="flex">
+				<div @dragover.prevent @drop="(ev) => handleDrop(ev, index)" class="bg-blue h-full">
+					drop
+				</div>
+				<FormModal reset-on-close @submit="(data) => editModule(module, data)">
+					<template v-slot:form-elements>
+						<ModuleForm :module="module" />
+					</template>
 
-				<template v-slot:open-button>
-					<ModuleCard :module="module" />
-				</template>
-			</FormModal>
+					<template v-slot:open-button>
+						<ModuleCard :module="module" />
+					</template>
+				</FormModal>
+			</div>
+			<div @dragover.prevent @drop="(ev) => handleDrop(ev)" class="bg-blue h-full">drop</div>
 		</div>
 	</div>
 </template>
