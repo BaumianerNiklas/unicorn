7a4b0592eb5b191bce8df8f0e9e155267706a8f7
BaumianerNiklas <o.niklas@tuta.io>
2024-03-18 20:41:04 +0100

    Add WithTooltip component

---
+++ src/components/WithTooltip.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import draggedModule from "@/data/draggedModule";
+import { ref } from "vue";
+
+const { tooltip } = defineProps<{ tooltip: string }>();
+
+let show = ref(false);
+</script>
+
+<template>
+	<div @mouseenter="show = true" @mouseleave="show = false" class="relative w-fit z-50">
+		<span
+			v-if="show && !draggedModule"
+			class="absolute bottom-4 left-[50%] translate-x-[-50%] text-xs bg-gray-700 text-white p-1 rounded-2"
+		>
+			{{ tooltip }}
+		</span>
+		<slot />
+	</div>
+</template>
