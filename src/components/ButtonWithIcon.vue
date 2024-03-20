a8c4bd773ef259aeb5ec49861a0aa1809088f78b
BaumianerNiklas <o.niklas@tuta.io>
2024-03-20 21:13:03 +0100

    add ButtonWithIcon component

---
+++ src/components/ButtonWithIcon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { text, icon } = defineProps<{
+	text: string;
+	icon: string;
+}>();
+</script>
+
+<template>
+	<button
+		class="flex items-center border-none text-base gap-1 py-1 px-2 bg-blue-600 hover:bg-blue-700 rounded-2 text-white cursor-pointer"
+	>
+		<div class="text-lg" :class="icon"></div>
+		<span>{{ text }}</span>
+	</button>
+</template>
