4313498b5e02b2667d3ed8be4eb322ebb4dd5f03
BaumianerNiklas <o.niklas@tuta.io>
2024-03-25 18:52:14 +0100

    make "your semesters" a heading and fix spelling

--- src/components/SemesterPlan.vue
+++ src/components/SemesterPlan.vue
@@ -5,7 +5,7 @@ import SemesterRow from "./SemesterRow.vue";
 
 <template>
 	<div>
-		your semester
+		<h2>Your semesters</h2>
 		<div class="flex flex-col">
 			<SemesterRow
 				v-for="semester in semesterCount"
