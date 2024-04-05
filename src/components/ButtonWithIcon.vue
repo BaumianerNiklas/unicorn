6a4e440537dc2e324383abab92d7ce609aa3d3f3
BaumianerNiklas <o.niklas@tuta.io>
2024-04-05 15:50:59 +0200

    add classList prop to ButtonWithIcon for customizing styles

--- src/components/ButtonWithIcon.vue
+++ src/components/ButtonWithIcon.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-const { text, icon } = defineProps<{
+const { text, icon, classList } = defineProps<{
 	text: string;
 	icon: string;
+	classList?: string;
 }>();
 </script>
 
 <template>
 	<button
 		class="flex items-center border-none text-base gap-1 py-1 px-2 bg-blue-600 hover:bg-blue-700 rounded-2 text-white cursor-pointer"
+		:class="classList"
 	>
 		<div class="text-lg" :class="icon"></div>
 		<span>{{ text }}</span>
