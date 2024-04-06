3ed5fbd68109e57a6a3efbba3b77157d964d3ed6
BaumianerNiklas <o.niklas@tuta.io>
2024-04-06 14:41:36 +0200

    prompt user for confirmation before closing with unsaved changes

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,14 @@ import TheMenu from "@/components/TheMenu.vue";
 import SemesterPlan from "@/components/SemesterPlan.vue";
 import { onBeforeMount } from "vue";
 import { loadFromLocalstorage } from "./util/localStorage";
+import hasUnsavedChanges from "./data/hasUnsavedChanges";
 
 onBeforeMount(() => loadFromLocalstorage());
+
+// Prompt user for confirmation when closing with unsaved changes
+window.addEventListener("beforeunload", (event) => {
+	if (hasUnsavedChanges.value) event.preventDefault();
+});
 </script>
 
 <template>
